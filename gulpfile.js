f18adca857bd093b0ef106b0d5a2f4c04adb46ab
ramzes1x3 <ramzes1x1@gmail.com>
2022-01-06 13:50:37 +0300

    general structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,20 @@
 const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
-const less = require("gulp-less");
+const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+let source_folder = "source";
 
 // Styles
 
 const styles = () => {
-  return gulp.src("source/less/style.less")
+  return gulp.src("source/scss/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(sync.stream());
@@ -42,8 +41,9 @@ exports.server = server;
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch(source_folder + "/scss/**/*.scss", gulp.series("styles"));
+  gulp.watch(source_folder + "/*.html").on("change", sync.reload);
+  gulp.watch(source_folder + "/js/script.js");
 }
 
 exports.default = gulp.series(
